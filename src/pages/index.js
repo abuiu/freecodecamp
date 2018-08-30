168eb1b2b81f5ab5756d000bf382dbd7f9ba3d13
Andrei Vader <andrei.buiu@gmail.com>
2018-08-30 06:51:02 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi guys!</h1>
+    <p>My name is Andrei Buiu.</p>
+    <p>I am a Software Engineer.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage;
